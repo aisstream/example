affaec993df7c0dc3a63fd5c79aa83568a85327f
aisstream.io <stream@aisstreamer.com>
2022-12-29 21:33:47 -0500

    remove invalidated aopi key

--- javascript/index.js
+++ javascript/index.js
@@ -3,7 +3,7 @@ const socket = new WebSocket("wss://stream.aisstream.io/v0/stream")
 
 socket.onopen = function (_) {
     let subscriptionMessage = {
-        Apikey: "8b84d262ed7e0cc54964afda265678232f656f29",
+        Apikey: "<YOUR API KEY>",
         BoundingBoxes: [[[-180, -90], [180, 90]]]
     }
     socket.send(JSON.stringify(subscriptionMessage));
